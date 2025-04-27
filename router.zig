88e628a0e6c81ea56a62b13553432d3755ad5a2a
alxgh <mmghk20@gmail.com>
2025-04-27 18:50:11 +0200

    better code structure + context struct

--- router.zig
+++ router.zig
@@ -1,21 +1,43 @@
 const std = @import("std");
 const tree = @import("tree.zig");
 const response = @import("response.zig");
+const http = std.http;
 
+const Allocator = std.mem.Allocator;
 const RoutesTree = tree.Radix(Route);
 const Handler = fn (req: *Request) anyerror!void;
+
+const Context = struct {
+    const ContextSelf = @This();
+    params: std.StringHashMap([]const u8),
+
+    fn init(allocator: Allocator) ContextSelf {
+        return .{
+            .params = std.StringHashMap([]const u8).init(allocator),
+        };
+    }
+
+    pub fn add(self: *ContextSelf, key: []const u8, val: []const u8) !void {
+        try self.params.put(key, val);
+    }
+
+    pub fn deinit(self: *ContextSelf) void {
+        self.params.deinit();
+    }
+};
 
 pub const Request = struct {
     const RequestSelf = @This();
 
-    req: std.http.Server.Request,
+    req: http.Server.Request,
     conn: std.net.Server.Connection,
     res: response.Response,
     allocator: std.mem.Allocator,
+    context: *Context,
 
-    pub fn init(allocator: std.mem.Allocator, conn: std.net.Server.Connection) !RequestSelf {
+    pub fn init(allocator: std.mem.Allocator, conn: std.net.Server.Connection, context: *Context) !RequestSelf {
         var buffer: [1024]u8 = undefined;
-        var http_server = std.http.Server.init(conn, &buffer);
+        var http_server = http.Server.init(conn, &buffer);
         const req = try http_server.receiveHead();
 
         return .{
@@ -23,19 +45,21 @@ pub const Request = struct {
             .req = req,
             .res = response.Response.init(allocator, req),
             .allocator = allocator,
+            .context = context,
         };
     }
 
     pub fn deinit(self: *RequestSelf) void {
         self.conn.stream.close();
         self.res.deinit();
+        self.context.deinit();
     }
 
     pub fn target(self: RequestSelf) []const u8 {
         return self.req.head.target;
     }
 
-    pub fn method(self: RequestSelf) std.http.Method {
+    pub fn method(self: RequestSelf) http.Method {
         return self.req.head.method;
     }
 
@@ -53,7 +77,7 @@ pub const Request = struct {
 const Route = struct {
     path: []const u8,
     handler: *const Handler,
-    method: std.http.Method,
+    method: http.Method,
 };
 const Self = @This();
 
@@ -126,8 +150,8 @@ pub fn delete(self: *Self, path: []const u8, handler: *const Handler) !void {
     });
 }
 
-fn add_route(self: *Self, route: Route) !void {
-    var route_tree = switch (route.method) {
+fn routes_tree(self: *Self, method: http.Method) RoutesTree {
+    return switch (method) {
         .GET => self.get_routes,
         .POST => self.post_routes,
         .PATCH => self.patch_routes,
@@ -135,8 +159,12 @@ fn add_route(self: *Self, route: Route) !void {
         .DELETE => self.delete_routes,
         else => unreachable,
     };
+}
 
-    try route_tree.insert(route.path, route);
+fn add_route(self: *Self, route: Route) !void {
+    var rt = self.routes_tree(route.method);
+
+    try rt.insert(route.path, route);
 }
 
 pub fn run(self: *Self) !void {
@@ -146,19 +174,23 @@ pub fn run(self: *Self) !void {
 }
 
 fn handleConnection(self: *Self, conn: std.net.Server.Connection) !void {
-    var request = try Request.init(self.allocator, conn);
+    const context = try self.allocator.create(Context);
+    context.* = Context.init(self.allocator);
+    var request = try Request.init(self.allocator, conn, context);
     defer request.deinit();
 
-    // var reader = try req.reader();
     std.debug.print("{s}|||{}\n", .{ request.target(), request.method() });
-    // reader.readAllAlloc(self.allocator, 0);
-    if (request.method() == .GET) {
-        const route = self.get_routes.lookup(request.target());
-        if (route) |r| {
-            try r.handler(&request);
-            return;
-        }
-    }
 
-    try request.req.respond("Not found", std.http.Server.Request.RespondOptions{ .status = .not_found });
+    try self.exec(&request);
+
+    try request.req.respond("Not found", http.Server.Request.RespondOptions{ .status = .not_found });
+}
+
+fn exec(self: *Self, request: *Request) !void {
+    var rt = self.routes_tree(request.method());
+    const route = rt.lookup(request.target());
+    if (route) |r| {
+        try r.handler(request);
+        return;
+    }
 }
